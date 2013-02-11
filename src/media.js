d69900f869bebe88557788ad735009a6567e9fe8
kikito <kikito@gmail.com>
2013-02-11 01:18:15 +0100

    added Image to Luv.Media

--- src/media.js
+++ src/media.js
@@ -1,40 +1,21 @@
-
-var Resource = {
-  isPending: function() { return this.status == "pending"; },
-  isLoaded:  function() { return this.status == "loaded"; },
-  isError:   function() { return this.status == "error"; }
-};
-
-
 Luv.Media = function() {
   this.pending = 0;
 
   var media = this;
 
-  media.Resource = function(source, loadCallback, errorCallback) {
-    media.pending++;
+  media.Image = function(src, loadCallback, errorCallback) {
+    var image = this,         // Luv image
+        source = new Image(); // html image
 
-    var resource = this;
-    resource.source = source;
-    resource.status = "pending";
+    Luv.Media.Resource.call(this, media, source, loadCallback, errorCallback);
 
-    resource.registerLoad = function() {
-      media.pending--;
-      resource.status = "loaded";
-      if(loadCallback) { loadCallback(resource); }
-      media.onResourceLoaded(resource);
-      if(media.isLoaded()) { media.onLoaded(); }
-    };
+    source.addEventListener('load',  function() { image.registerLoad(); });
+    source.addEventListener('error', function() { image.registerError(); });
 
-    resource.registerError = function(evt) {
-      media.pending--;
-      resource.status = "error";
-      if(errorCallback) { errorCallback(resource); }
-      media.onLoadError(resource, evt);
-    };
+    this.source.src = src;
   };
+  media.Image.prototype = Luv.Media.Resource.prototype;
 
-  media.Resource.prototype = Resource;
 };
 
 var media = Luv.Media.prototype;
@@ -44,3 +25,34 @@ media.getPending          = function() { return this.pending; };
 media.onResourceLoaded    = function(resource) {};
 media.onLoadError         = function(resource) { throw new Error("Could not load " + resource); };
 media.onLoaded            = function() {};
+
+////
+
+Luv.Media.Resource = function(media, source, loadCallback, errorCallback) {
+  media.pending++;
+
+  this.media          = media;
+  this.source         = source;
+  this.loadCallback   = loadCallback;
+  this.errorCallback  = errorCallback;
+  this.status         = "pending";
+};
+
+Luv.Media.Resource.prototype = {
+  registerLoad: function() {
+    this.media.pending--;
+    this.status = "loaded";
+    if(this.loadCallback) { this.loadCallback(this); }
+    this.media.onResourceLoaded(this);
+    if(this.media.isLoaded()) { this.media.onLoaded(); }
+  },
+  registerError: function(evt) {
+    this.media.pending--;
+    this.status = "error";
+    if(this.errorCallback) { this.errorCallback(this); }
+    this.media.onLoadError(this);
+  },
+  isPending: function() { return this.status == "pending"; },
+  isLoaded:  function() { return this.status == "loaded"; },
+  isError:   function() { return this.status == "error"; }
+};
--- test/luv/media.js
+++ test/luv/media.js
@@ -1,103 +1,128 @@
+describe("Luv.Media.Image", function() {
+  var media;
+  beforeEach(function(){ media = new Luv.Media(); });
+
+  it("exists", function(){
+    expect(media.Image).to.be.a('function');
+  });
+
+  it("invokes a custom callback when loaded", function(){
+    var callback = sinon.spy();
+    var image = new media.Image(null, callback);
+    trigger(image.source, 'load');
+    expect(callback).to.have.been.calledWith(image);
+    expect(image.isLoaded()).to.be.True;
+  });
+
+  it("invokes a custom callback when fails to load", function(){
+    sinon.stub(media, 'onLoadError');
+    var callback = sinon.spy();
+    var image = new media.Image(null, null, callback);
+    trigger(image.source, 'error');
+    expect(callback).to.have.been.calledWith(image);
+    expect(image.isError()).to.be.True;
+  });
+});
+
+describe("Luv.Media.Resource", function() {
+  var media;
+  beforeEach(function(){ media = new Luv.Media(); });
+
+  it("exists", function(){
+    expect(Luv.Media.Resource).to.be.a('function');
+  });
+
+  it("invokes a custom callback when a resource is loaded", function(){
+    var callback = sinon.spy();
+    var resource = new Luv.Media.Resource(media, {}, callback);
+    resource.registerLoad();
+    expect(callback).to.have.been.calledWith(resource);
+    expect(resource.isLoaded()).to.be.True;
+  });
+
+  it("invokes a custom callback when a resource fails to load", function(){
+    sinon.stub(media, 'onLoadError');
+    var callback = sinon.spy();
+    var resource = new Luv.Media.Resource(media, {}, null, callback);
+    resource.registerError();
+    expect(callback).to.have.been.calledWith(resource);
+    expect(resource.isError()).to.be.True;
+  });
+});
+
 describe("Luv.Media", function(){
+  var media;
+  beforeEach(function(){ media = new Luv.Media(); });
+
   it("exists", function(){
     expect(Luv.Media).to.be.a('function');
   });
 
-  describe(".methods", function(){
-    var media;
-    beforeEach(function(){
-      media   = new Luv.Media();
+  describe(".onResourceLoaded(resource)", function(){
+    it("is called when a new resource is loaded", function() {
+      var onResourceLoaded = sinon.spy(media, 'onResourceLoaded');
+      var resource = new Luv.Media.Resource(media, {});
+      resource.registerLoad();
+      expect(onResourceLoaded).to.have.been.calledWith(resource);
+    });
+  });
+
+  describe(".onLoadError(resource)", function(){
+    it("throws an error by default", function() {
+      var peter = {toString: function(){return "Peter"; }};
+      expect(function(){ media.onLoadError(peter); }).to.Throw(Error);
     });
 
-    describe("Resource", function() {
-      it("exists", function(){
-        expect(media.Resource).to.be.a('function');
-      });
-
-      it("invokes a custom callback when a resource is loaded", function(){
-        var callback = sinon.spy();
-        var resource = new media.Resource({}, callback);
-        resource.registerLoad();
-        expect(callback).to.have.been.calledWith(resource);
-        expect(resource.isLoaded()).to.be.True;
-      });
-
-      it("invokes a custom callback when a resource fails to load", function(){
-        sinon.stub(media, 'onLoadError');
-        var callback = sinon.spy();
-        var resource = new media.Resource({}, null, callback);
-        resource.registerError();
-        expect(callback).to.have.been.calledWith(resource);
-        expect(resource.isError()).to.be.True;
-      });
+    it("is called when a new resource fails to load", function() {
+      var onLoadError = sinon.stub(media, 'onLoadError');
+      var resource = new Luv.Media.Resource(media, {});
+      resource.registerError();
+      expect(onLoadError).to.have.been.calledWith(resource);
     });
+  });
 
-    describe(".onResourceLoaded(resource)", function(){
-      it("is called when a new resource is loaded", function() {
-        var onResourceLoaded = sinon.spy(media, 'onResourceLoaded');
-        var resource = new media.Resource({});
-        resource.registerLoad();
-        expect(onResourceLoaded).to.have.been.calledWith(resource);
-      });
+  describe(".onLoaded()", function(){
+    it("is called when all resources are loaded", function() {
+      var onLoaded = sinon.spy(media, 'onLoaded'),
+          r1       = new Luv.Media.Resource(media, {}),
+          r2       = new Luv.Media.Resource(media, {});
+      r1.registerLoad();
+      r2.registerLoad();
+
+      expect(onLoaded).to.have.been.calledOnce;
     });
+  });
 
-    describe(".onLoadError(resource)", function(){
-      it("throws an error by default", function() {
-        var peter = {toString: function(){return "Peter"; }};
-        expect(function(){ media.onLoadError(peter); }).to.Throw(Error);
-      });
+  describe(".isLoaded()", function(){
+    it("returns true when all pending resources are loaded", function() {
+      expect(media.isLoaded()).to.equal(true);
+      var r1       = new Luv.Media.Resource(media, {}),
+          r2       = new Luv.Media.Resource(media, {});
 
-      it("is called when a new resource fails to load", function() {
-        var onLoadError = sinon.stub(media, 'onLoadError');
-        var resource = new media.Resource({});
-        resource.registerError();
-        expect(onLoadError).to.have.been.calledWith(resource);
-      });
+      expect(media.isLoaded()).to.equal(false);
+
+      r1.registerLoad();
+      expect(media.isLoaded()).to.equal(false);
+
+      r2.registerLoad();
+      expect(media.isLoaded()).to.equal(true);
     });
+  });
 
-    describe(".onLoaded()", function(){
-      it("is called when all resources are loaded", function() {
-        var onLoaded = sinon.spy(media, 'onLoaded'),
-            r1       = new media.Resource({}),
-            r2       = new media.Resource({});
-        r1.registerLoad();
-        r2.registerLoad();
+  describe(".getPending()", function(){
+    it("is called when all ", function() {
+      expect(media.getPending()).to.equal(0);
+      var r1       = new Luv.Media.Resource(media, {}),
+          r2       = new Luv.Media.Resource(media, {});
 
-        expect(onLoaded).to.have.been.calledOnce;
-      });
+      expect(media.getPending()).to.equal(2);
+
+      r1.registerLoad();
+      expect(media.getPending()).to.equal(1);
+
+      r2.registerLoad();
+      expect(media.getPending()).to.equal(0);
     });
+  });
 
-    describe(".isLoaded()", function(){
-      it("returns true when all pending resources are loaded", function() {
-        expect(media.isLoaded()).to.equal(true);
-        var r1       = new media.Resource({}),
-            r2       = new media.Resource({});
-
-        expect(media.isLoaded()).to.equal(false);
-
-        r1.registerLoad();
-        expect(media.isLoaded()).to.equal(false);
-
-        r2.registerLoad();
-        expect(media.isLoaded()).to.equal(true);
-      });
-    });
-
-    describe(".getPending()", function(){
-      it("is called when all ", function() {
-        expect(media.getPending()).to.equal(0);
-        var r1       = new media.Resource({}),
-            r2       = new media.Resource({});
-
-        expect(media.getPending()).to.equal(2);
-
-        r1.registerLoad();
-        expect(media.getPending()).to.equal(1);
-
-        r2.registerLoad();
-        expect(media.getPending()).to.equal(0);
-      });
-    });
-
-  }); // methods
 });
