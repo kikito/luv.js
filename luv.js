16180bf6289bfedceb8a8c4c1dcd62a3fa9855cb
kikito <kikito@gmail.com>
2013-03-01 17:07:37 +0100

    improved mouse.js docs

--- luv.js
+++ luv.js
@@ -459,13 +459,13 @@ var getKeyFromEvent = function(event) {
 
 // ## Luv.Mouse
 Luv.Mouse = function(el) {
-  // This module allows luv.js games to get input from the mouse
+  // This function creates a mouse handler for a mouse game.
   // It is usually instantiated directly by the main Luv() function,
-  // a programmer using luv.js to create a game will almost certainly
-  // not need to create this module. But it's completely possible to
-  // do so, like this:
+  // you will probably not need to call `Luv.Mouse()` yourself:
 
-  //       var mouse = Luv.Mouse(el); // el must be a DOM element
+  //       var luv = Luv();
+  //       luv.mouse // Already instantiated mouse handler
+
   var mouse = Object.create(MouseProto);
 
   mouse.x = 0;
@@ -508,13 +508,42 @@ var getButtonFromEvent = function(evt) {
 // ## MouseProto
 // Shared mouse functions go here
 var MouseProto = {
+
+  // Returns the x coordinate where the mouse is (relative to the DOM element)
   getX: function() { return this.x; },
+
+  // Returns the x coordinate where the mouse is (relative to the DOM element)
   getY: function() { return this.y; },
+
+  // Returns both the x and y coordinates of the mouse, as an object of the form
+  // `{x: 100, y:200}
   getPosition: function() {
     return {x: this.x, y: this.y};
   },
+
+  // `onPressed` is an overridable callback that is called when any of the mouse
+  // buttons is pressed.
+  // `button` is a string representing a button name (`"l"` for left, `"m"` for middle,
+  // `"r"` for right).
+
+  //       var luv = Luv();
+  //       luv.mouse.onPressed = function(x,y,button) {
+  //         console.log("Mouse button " + button + " was pressed in " + x + ", " + y);
+  //       }
   onPressed: function(x,y,button) {},
+
+  // Works the same as `onPressed`, but is called when a button stops being pressed.
   onReleased: function(x,y,button) {},
+
+  // Returns true if a button is pressed, false otherwhise. Usually used inside
+  // the update loop:
+
+  //       var luv = Luv();
+  //       luv.update = function(dt) {
+  //         if(luv.mouse.isPressed('l')) {
+  //           console.log('Left mouse button pressed');
+  //         }
+  //       };
   isPressed: function(button) {
     return !!this.pressedButtons[button];
   }
