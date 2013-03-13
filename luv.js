19b8e4d5b7838af1a63de5aff30d607fba64c735
kikito <kikito@gmail.com>
2013-03-14 00:51:39 +0100

    fix tests for Luv.create

--- luv.js
+++ luv.js
@@ -1,4 +1,4 @@
-/*! luv 0.0.1 (2013-03-13) - https://github.com/kikito/luv.js */
+/*! luv 0.0.1 (2013-03-14) - https://github.com/kikito/luv.js */
 /*! Minimal HTML5 game development lib */
 /*! Enrique Garcia Cota */
 // #shims.js
