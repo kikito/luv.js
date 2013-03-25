fd01a635a49ccecd3dc57ea5ebbb7eb9a17636a8
kikito <kikito@gmail.com>
2013-03-25 19:50:49 +0100

    animations now accept delays in a variety of ways

--- luv.js
+++ luv.js
@@ -1398,12 +1398,61 @@ Luv.Graphics.Animation = Luv.Class('Luv.Graphics.Animation', {
 
 
 var parseDelays = function(length, delays) {
-  var result = [];
-  if(typeof delays == "number") {
-    for(var i=0; i<length; i++) {
+  var result=[], r, i, range, value;
+
+  if(Array.isArray(delays)) {
+    result = delays.slice(0);
+
+  } else if(typeof delays == "object") {
+    result.length = length;
+    for(r in delays) {
+      if(delays.hasOwnProperty(r)) {
+        range = parseRange(r);
+        value = Number(delays[r]);
+        for(i=0; i<range.length; i++) {
+          result[range[i]] = value;
+        }
+      }
+    }
+  } else {
+    delays = Number(delays);
+    for(i=0; i<length; i++) {
       result.push(delays);
     }
   }
+
+  if(result.length != length) {
+    throw new Error('The delays table length should be ' + length +
+                    ', but it is ' + result.length);
+  }
+
+  for(i=0; i<result.length; i++) {
+    if(typeof result[i] === "undefined") {
+      throw new Error('Missing delay for sprite ' + i);
+    }
+  }
+  return result;
+};
+
+var parseRange = function(r) {
+  var match, result, start, end, i;
+  if(typeof r != "string") {
+    throw new Error("Unknown range type (must be integer or string in the form 'start-end'): " + r);
+  }
+  match = r.match(/^(\d+)-(\d+)$/);
+  if(match) {
+    result = [];
+    start  = Number(match[1]);
+    end    = Number(match[2]);
+    if(start < end) {
+      for(i=start; i<=end; i++) { result.push(i); }
+    } else {
+      for(i=start; i>=end; i--) { result.push(i); }
+    }
+  } else {
+    result = [Number(r)];
+  }
+
   return result;
 };
 
--- test/src/graphics/animation.js
+++ test/src/graphics/animation.js
@@ -16,15 +16,53 @@ describe('Luv.Graphics.Animation', function() {
       expect(function(){ Luv.Graphics.Animation('loop', [], 1); }).to.Throw(Error);
     });
 
-    it('sets the mode and delay for each sprite', function() {
-      var anim = Luv.Graphics.Animation('loop', [1,2,3], 4);
-
-      expect([anim.mode, anim.timer, anim.position, anim.direction, anim.status, anim.sprites, anim.delays]).to.deep.equal(
-             ["loop",    0,          0,             1,              "playing",   [1,2,3],      [4,4,4] ]
-      );
-
+    describe('when a number is used for the delays', function() {
+      it('sets the same delays for all the sprites', function() {
+        var a = Luv.Graphics.Animation('loop', [1,2,3], 4);
+        expect([a.mode, a.timer, a.position, a.direction, a.status, a.sprites, a.delays]).to.deep.equal(
+               ["loop", 0,       0,          1,          "playing", [1,2,3],   [4,4,4] ]
+        );
+      });
     });
 
+    describe('when an array is used for the delays', function() {
+      it('throws an error if the delays has not the same length as the sprites', function() {
+        expect(function(){ Luv.Graphics.Animation('loop', [1,2,3], [1,2]); }).to.Throw(Error);
+        expect(function(){ Luv.Graphics.Animation('loop', [1,2,3], [1,2,3,4]); }).to.Throw(Error);
+      });
+      it('accepts the array values as delays', function() {
+        var a = Luv.Graphics.Animation('loop', [1,2,3], [4,5,6]);
+        expect([a.mode, a.timer, a.position, a.direction, a.status, a.sprites, a.delays]).to.deep.equal(
+               ["loop", 0,       0,          1,          "playing", [1,2,3],   [4,5,6] ]
+        );
+      });
+    });
+
+    describe('when an object is used for the delays', function() {
+
+      describe('when the keys are integers', function() {
+        it('uses the values as delays', function() {
+          var a = Luv.Graphics.Animation('loop', [1,2,3], {0: 4, 1: 5, 2: 6});
+          expect([a.mode, a.timer, a.position, a.direction, a.status, a.sprites, a.delays]).to.deep.equal(
+                 ["loop", 0,       0,          1,          "playing", [1,2,3],   [4,5,6] ]
+          );
+        });
+      });
+
+      describe('when the keys are strings', function() {
+        it('accepts ranges', function() {
+          var a = Luv.Graphics.Animation('loop', [1,2,3,4], {'0-2': 2, 3: 3});
+          expect([a.mode, a.timer, a.position, a.direction, a.status, a.sprites, a.delays]).to.deep.equal(
+                 ["loop", 0,       0,          1,          "playing", [1,2,3,4],   [2,2,2,3] ]
+          );
+        });
+      });
+
+      it('throws an error if there are missing delays or too many delays', function() {
+        expect(function(){ Luv.Graphics.Animation('loop', [1,2,3], {'0-1': 1}); }).to.Throw(Error);
+        expect(function(){ Luv.Graphics.Animation('loop', [1,2,3], {'0-10': 1}); }).to.Throw(Error);
+      });
+    });
 
 
   });
