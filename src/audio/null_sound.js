1fe9588166765d3c94ccccc01505632db71ebb02
kikito <kikito@gmail.com>
2013-03-29 14:12:55 +0100

    defined null_sound in terms of sound

--- src/audio/null_sound.js
+++ src/audio/null_sound.js
@@ -8,27 +8,27 @@
 //
 // Browsers which don't support the `<audio>` tag will instantiate this class instead
 // of Luv.Audio.Sound.
-Luv.Audio.NullSound = Luv.Class('Luv.Audio.NullSound', {
-  // `play` creates and returns a `SoundInstance` whose internal element is a "fake"
-  // audio tag (in reality, a plain js object).
+Luv.Audio.NullSound = Luv.Class('Luv.Audio.NullSound');
+
+// Implement all the methods of Luv.Audio.Sound, but just return 0 instead of
+// doing anything.
+var fakeMethods = {},
+    fakeMethod = function() { return 0; };
+for(var name in Luv.Audio.Sound.methods) {
+  if(Luv.Audio.Sound.methods.hasOwnProperty(name)) {
+    fakeMethods[name] = fakeMethod;
+  }
+}
+
+Luv.Audio.NullSound.include(fakeMethods, {
+  // `play` is the only method that does something in a NullSound (it still does very little)
+  // It returns a `SoundInstance` whose internal element is a "fake" audio tag (in reality, a plain js object).
   // This makes it interface-compatible with Luv.Audio.Sound, but with no side effects,
   // since the sound instances it produces do nothing.
   play: function() {
     return Luv.Audio.SoundInstance(FakeAudioElement());
   }
 });
-
-var nopFunctions = {};
-"pause stop setVolume setVolume setLoop setSpeed setTime setExpirationTime".split(" ").forEach(function(name){
-  nopFunctions[name] = function(){};
-});
-
-var zeroFunctions = {};
-"countInstances countPlayingInstances getExpirationTime getVolume getSpeed getTime getDuration".split(" ").forEach(function(name){
-  nopFunctions[name] = function(){ return 0; };
-});
-
-Luv.Audio.NullSound.include(nopFunctions, zeroFunctions);
 
 var FakeAudioElement = function() {
   return {
