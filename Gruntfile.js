e292973cd1414fd40e01fb8ab45422a2fa70261e
kikito <kikito@gmail.com>
2013-04-01 17:21:59 +0200

    semi-working example page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
   var shell = require('shelljs');
+  var fs    = require('fs');
 
   // order of these files is important, that's why you can't do src/*
   var srcFiles  = [
@@ -35,6 +36,26 @@ module.exports = function(grunt) {
       "mv " + output + "/README.js.html " + output + "/index.html",
       "rm README.js.md"
     ]);
+  };
+
+
+  var generateExamples = function(output) {
+    var isExample  = function(path) { return (/\.html$/).test(path) && path != "index.html"; },
+        removeHtml = function(path) { return path.slice(0, -5); },
+        humanize   = function(str)  { return str.replace(/_/g, " "); },
+        makeUpper  = function(str)  { return str.toUpperCase(); },
+        capitalize = function(str)  { return str.replace(/(^| )(\w)/g, makeUpper); },
+        examples   = [];
+
+    fs.readdirSync('examples').filter(isExample).sort().forEach(function(filename) {
+      var id   = removeHtml(filename),
+          name = capitalize(humanize(id)),
+          html = fs.readFileSync('examples/' + filename, 'utf8');
+
+      examples.push({id: id, name: name, html: html});
+    });
+
+    fs.writeFileSync(output + "/examples.js", 'var examples = ' + JSON.stringify(examples) + ';');
   };
 
   grunt.initConfig({
@@ -86,6 +107,10 @@ module.exports = function(grunt) {
 
   grunt.registerTask('docs', 'Runs docco and exports the result to docs', function() {
     generateDocco('docs');
+  });
+
+  grunt.registerTask('examples', 'generates examples/index.html', function() {
+    generateExamples('examples');
   });
 
   grunt.registerTask('gh-pages', 'generates the docs with docco and publishes to github pages', function() {
