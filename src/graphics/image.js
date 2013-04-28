dc009bc4afdf31259d001803d38cacd73a26804c
kikito <kikito@gmail.com>
2013-04-28 14:56:27 +0200

    added docs to image

--- src/graphics/image.js
+++ src/graphics/image.js
@@ -4,6 +4,15 @@
 // ## Luv.Graphics.Image
 // This class encapsulates images loaded from the internet
 Luv.Graphics.Image = Luv.Class('Luv.Graphics.Image', {
+
+  // Usually you will not instantiate images directly. Instead, you will use
+  // the instance method of Luv.Graphics, like this:
+  //
+  //       var luv = Luv(...);
+  //       var image = luv.graphics.Image('face.png');
+  //
+  // Notice that this constructor requires an additional parameter (an instance
+  // of Luv.Media), which is provided automatically by luv.graphics.Image.
   init: function(media, path) {
     var image = this;
 
@@ -44,6 +53,7 @@ Luv.Graphics.Image = Luv.Class('Luv.Graphics.Image', {
 
 });
 
+// Add methods like isLoaded, isError, isPending to image
 Luv.Graphics.Image.include(Luv.Media.Asset);
 
 }());
