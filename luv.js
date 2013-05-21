22cdbdaaaa37a4e049eb2cc578c20bee9266de1b
kikito <kikito@gmail.com>
2013-05-21 19:01:25 +0200

    added isIntersecting

--- luv.js
+++ luv.js
@@ -3105,10 +3105,17 @@ Luv.Collider.AABB = Luv.Class('Luv.Collider.AABB', {
     this.l   = l;
     this.w   = w;
     this.h   = h;
+    this.r   = l + w;
+    this.b   = t + h;
     this.w2  = w / 2;
     this.h2  = h / 2;
     this.x   = l + this.w2;
     this.y   = t + this.h2;
+  },
+
+  isIntersecting: function(other) {
+    return this.l < other.r && this.r > other.l &&
+           this.t < other.b && this.b > other.t;
   }
 
 });
--- test/src/collider/aabb.js
+++ test/src/collider/aabb.js
@@ -11,10 +11,17 @@ describe('Luv.Collider.AABB', function() {
       var b = AABB(10,20,100,200);
 
       expect([
-        b.l, b.t, b.w, b.h, b.x, b.y, b.w2, b.h2
+        b.l, b.t, b.w, b.h, b.r, b.b, b.x, b.y, b.w2, b.h2
       ]).to.deep.equal([
-        10,   20, 100, 200,  60,  120, 50, 100
+        10,   20, 100, 200,  110, 220, 60,  120, 50, 100
       ]);
+    });
+  });
+
+  describe('isIntersecting', function() {
+    it("checks whether two AABBs are intersecting", function() {
+      expect(AABB(10,20,100,200).isIntersecting(AABB(150,200,10,10))).to.equal(false);
+      expect(AABB(10,20,100,200).isIntersecting(AABB(15,25,100,200))).to.equal(true);
     });
   });
 
